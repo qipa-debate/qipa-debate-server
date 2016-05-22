5dfcd7e8f0001d5d469ae8730229f04c002af86a
liuchang890726 <liuchang890726@hotmail.com>
2016-05-22 08:48:11 +0800

    use pyro4

--- Detector/detector.py
+++ Detector/detector.py
@@ -2,47 +2,60 @@ import os
 import sys
 import time
 import threading
-import socket
+import Pyro4
 import commands
 
 file_path = sys.argv[1]
 file_list = []
-lock = threading.RLock()
-words = ""
-content = ""
+
+class Detector:
+	def __init__(self):
+		self.lock = threading.RLock()
+		self.words = ""
+		self.content = ""
+
+	def get_content(self):
+		with self.lock:
+			content_t = self.content
+			self.content = ""
+		return content_t
+
+	def get_words(self):
+		with self.lock:
+			words_t = self.words
+			self.words = ""
+
+	def set_content(self, c):
+		with self.lock:
+			self.content = self.content + "\n" + c
+			print self.content
+
+	def set_words(self, w):
+		with self.lock:
+			self.words = w
+			print self.words
+
+detector = Detector()
 
 def run():
 	while True:
+		port = 8080
 		try:
-			s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-			s.bind(("0.0.0.0",8090))
-			s.listen(1)
-			while True:
-				conn,addr = s.accept()
-				print 'Connected by', addr
-				data = conn.recv(1024)
-				if data == "content":
-					with lock:
-						conn.sendall(len(content))
-						conn.sendall(content)
-						content = ""
-				elif data == "words":
-					with lock:
-						conn.sendall(len(words))
-						conn.sendall(words)
-						words = ""
-				conn.close()
-		except Exception, e:
-			print e
-		finally:
-			s.close()
+			daemon = Pyro4.Daemon("0.0.0.0", port)                # make a Pyro daemon
+			uri = daemon.register(detector)   # register the greeting maker as a Pyro object
+
+			print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
+			daemon.requestLoop() 
+		except:
+			port = port + 1
+			if port > 8090:
+				port = 8080
 
 thread = threading.Thread(target = run)
 thread.setDaemon(True)
 thread.start()
 
 while True:
-	print "%s, %s, %s" % (file_list, content, words)
 	try:
 		temp_file = []
 		for root, _, files in os.walk(file_path):
@@ -59,14 +72,11 @@ while True:
 			print file_prefix
 
 			print os.system("sh run.sh " + file_prefix)
-			with lock:
-				with open(file_prefix + ".content", "r") as fp:
-					content = content + "\n" + fp.read().decode("utf-8")
-					print content
-				
-				with open(file_prefix + ".words", "r") as fp:
-					words = fp.read().decode("utf-8")
-					print words
+			with open(file_prefix + ".content", "r") as fp:
+				detector.set_content(fp.read().decode("utf-8"))
+			
+			with open(file_prefix + ".words", "r") as fp:
+				detector.set_words(fp.read().decode("utf-8"))
 
 			file_list.append(temp_file[0][1])
 	except Exception, e:
--- Transmitter/transmitter.py
+++ Transmitter/transmitter.py
@@ -2,10 +2,11 @@ import time
 import datetime
 import sys
 import pickle
+import Pyro4
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-if len(sys.argv) > 1:
+def add_to_shimo(content):
 	driver = webdriver.Chrome(sys.argv[1])  # Optional argument, if not specified will search path.
 	#driver.add_cookie({'name' : 'session', 'value' : 'eyJfaWQiOnsiIGIiOiJaalUyTm1aak5EVTRPRE5qWWpVNU9HWmtNalZoWmpBNE4yUTNaR016TkRrPSJ9fQ', 'path' : '/'})
 	driver.implicitly_wait(0)
@@ -14,11 +15,35 @@ if len(sys.argv) > 1:
 	for cookie in cookies:
 		driver.add_cookie(cookie)
 	driver.get('https://www.shimo.im/doc/g8XvnVEvCtEWnvrB')
-	search_box = driver.find_element_by_class_name('locate')
-	mouse = ActionChains(driver)
-	mouse.move_to_element_with_offset(search_box, 0, 0)
-	mouse.click()
-	mouse.send_keys(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ': BBB\n')
-	mouse.perform()
+	try:
+		search_box = driver.find_element_by_class_name('locate')
+		mouse = ActionChains(driver)
+		mouse.move_to_element_with_offset(search_box, 0, 0)
+		mouse.click()
+		mouse.send_keys(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ': %s\n' % content)
+		mouse.perform()
+	except:
+		mouse = ActionChains(driver)
+		mouse.move_by_offset(290, 400)
+		mouse.click()
+		mouse.send_keys(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ': %s\n' % content)
+		mouse.perform()
+		
 	time.sleep(5)
 	driver.quit()
+
+#
+if len(sys.argv) > 1:
+	while True:
+		print "*"
+		try:
+			uri = "PYRO:obj_06bd3d87dbb549e0871684bc5d7620ed@121.201.29.100:8080"
+			client = Pyro4.Proxy(uri)
+			content = client.get_content()
+			if content:
+				add_to_shimo(content)
+		except Exception, e:
+			print e
+		time.sleep(5)
+
+
